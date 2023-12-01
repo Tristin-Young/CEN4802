0be6fd51c80b85ec5eb6499bb852e282f1d13fab
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2023-11-30 20:25:04 -0500

    Some files did nto commit properly, secondary commit

--- Module2/src/Fibbonaci.java
+++ Module2/src/Fibbonaci.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Fibbonaci {
+//class to calculate fibbonaci numbers
+public class Fibbonaci {
+
+    //class to calculate fibbonaci numbers recursively
+    public static int fibbonaciRecursive(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fibbonaciRecursive(n-1) + fibbonaciRecursive(n-2);
+    }
 }
+
--- Module2/src/Main.java
+++ Module2/src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+        public static void main(String[] args) {
+            int n = 10;
+            //call fibbonaciRecursive method from Fibbonaci class
+            int result = Fibbonaci.fibbonaciRecursive(n);
+            System.out.println("Term " + n + " of the fibbonaci sequence is: " + result);
+        }
 }
